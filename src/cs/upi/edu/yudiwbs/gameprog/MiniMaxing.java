d315a57d7456778ec0bce9412480cedf7c0a49c8
yudiwbs <yudi1975@gmail.com>
2015-11-16 03:40:26 +0700

    perbaiki bug penghitungan skor board

--- src/cs/upi/edu/yudiwbs/gameprog/MiniMaxing.java
+++ src/cs/upi/edu/yudiwbs/gameprog/MiniMaxing.java
@@ -10,17 +10,16 @@ import java.util.ArrayList;
 
 public class MiniMaxing {
 
-    static final int MAXDEPTH = 100;
+    static final int MAXDEPTH = 8;
 
     static final int INFINITY = Integer.MAX_VALUE;
 
     public MoveScore minimax(Board mBoard,int player,int currentDepth ) {
-        MoveScore out = new MoveScore();
         MoveScore best = new MoveScore();
         //kondisi berhenti, gameover atau sudah terlalu dalam
-        if (mBoard.isGameOver() || currentDepth >= MAXDEPTH ) {
-            out.score = mBoard.evaluasi(player);
-            out.move = -1; //tidak ada move
+        if (mBoard.isGameOver() || currentDepth == MAXDEPTH ) {
+            best.score = mBoard.evaluasi(player);
+            best.move = -1; //tidak ada move
         } else
         {
             best.move = -1;
@@ -31,9 +30,15 @@ public class MiniMaxing {
 
             //loop untuk semua kemungkinan gerakan
             //for move in board.getMoves():
+
+            //System.out.println("debug mboard");
+            //mBoard.print();
             ArrayList<Integer> alMoves = mBoard.getMoves();
             for (int move:alMoves) {
                 Board newBoard = mBoard.makeMove(move);  //buat board yg baru, isinya sama dgn yg lama + move
+
+                System.out.println("debug new board:");
+                newBoard.print();
 
                 //currentScore, currentMove = minimax(newBoard, player,20 maxDepth, currentDepth+1)
                 MoveScore currentMS = minimax(newBoard,player,currentDepth+1); //rekursif
@@ -41,18 +46,25 @@ public class MiniMaxing {
                 if (mBoard.currentPlayer() == player) {
                     if  (currentMS.score > best.score) {
                         best.score = currentMS.score;
-                        best.move = currentMS.score;
+                        best.move = move;
                     }
                 } else  //lawan, cari minimal
                 {
                     if (currentMS.score < best.score) {
                         best.score =currentMS.score;
-                        best.move = currentMS.move;
+                        best.move = move;
                     }
                 }
             }
         }
-        return out;
+        //debug
+        /*
+        System.out.print("Best score:  ");
+        System.out.println(best.score);
+        System.out.print("Best Move:  ");
+        System.out.println(best.move);
+        */
+        return best;
     }
 
     public MoveScore getBestMove(Board mBoard,int player) {
@@ -64,7 +76,14 @@ public class MiniMaxing {
     public static void main(String[] args) {
         MiniMaxing mm = new MiniMaxing();
         Board b = new Board();
+        b.setCurrentPlayer(1);
+
+        b = b.makeMove(4);  //debug  player 1 main
         b.print();
+
+        MoveScore ms = mm.getBestMove(b,1);
+        ms.print();
+        System.out.println("selesai");
     }
 
 }
